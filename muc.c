fb7ad6fff7b6cfbed41cf88842b4c669b1c26625
Christoffer Eriksson <dv22cen@cs.umu.se>
2024-08-30 02:12:46 +0200

    Initial setup plus setting up class structure

--- muc.c
+++ muc.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(){
+//TODO helper class for allocating array for tokens
+//TODO helper klass tokenizer = token_reader
 
-    printf("Hello World!");
+int main(int argc, char *argv[]){
+    if(argc != 2 ){
+        printf("Wrong Usage.\n Correct usage is muc <input.mu>");
+    }
+
+    else{
+        printf("Lexical analysis starting");
+        token_reader(argv[1]);
+    }
 
     return 0;
+}
+
+char** token_reader(char* filename){
+    FILE* input = fopen(filename, 'r');
+    if(input != NULL){
+        char* buffer[50];
+        int read;
+        while (read = fgetc(input) != EOF)
+        {
+            
+        }
+        
+        fclose(input);
+    }
+
+    else{
+        printf("Could not open file");
+        fclose(input);
+    }
 }
