0789c0d9d7c883d6e10a4d93d6cb3d3833b96764
Christoffer Eriksson <dv22cen@cs.umu.se>
2024-11-20 17:04:57 +0100

    Removed old executables and fixed todos

--- muc.c
+++ muc.c
@@ -6,10 +6,8 @@
 #include "linked_list.h"
 #include "tokenizer.h"
 
-//TODO insert consume and peek into linked_list_print
-//TODO errorcheck list remove
-//TODO swap current setup for is_keyword and so forth.
-//TODO add same checking for operators
+//TODO split typeNode into smaller type nodes(Literals, functions etc.)
+//TODO swap current setup for is_keyword(DONE) and is_operator.
 //TODO add types for nodes in AST
 //TODO AST BUILDING
 //TODO update_token_type use length
