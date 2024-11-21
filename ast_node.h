91f756336d68ab37d8003a72b6bd966b90651c88
Christoffer Eriksson <dv22cen@cs.umu.se>
2024-11-21 21:49:06 +0100

    Changed exprNode struct to be simpler and more straightforward

--- ast_node.h
+++ ast_node.h
@@ -20,25 +20,24 @@ typedef enum {
     EXPR_FUNCTION_CALL // Function call like `foo(1, 2)`
 } ExprType;
 
-// Struct for more complex types
-typedef struct TypeNode {
-    TypeKind kind;              // The kind of type (basic, pointer, array, etc.)
-    struct TypeNode *subType;   // For pointers or arrays, points to the underlying type
-    int arraySize;              // Array size (used if kind == TYPE_ARRAY)
-    char *customTypeName;       // For custom types like structs
-    struct TypeNode **paramTypes; // For function types, a list of parameter types
-    int paramCount;             // Number of parameters for function types
-    struct TypeNode *returnType; // Return type for functions
-} TypeNode;
-
 typedef struct ExprNode {
     ExprType type;  // Specifies the type of expression
     union {
         struct {
             int intValue;
+        } int_literal;
+
+        struct {
             float floatValue;
+        } float_literal;
+
+        struct {
             char *stringValue;
-        } literal;           // For EXPR_LITERAL
+        } string_literal;
+
+        struct {
+            char charValue;
+        } char_literal;
 
         struct {
             char *varName;   // The name of the variable (for EXPR_VARIABLE)
@@ -49,21 +48,7 @@ typedef struct ExprNode {
             struct ExprNode *left;
             struct ExprNode *right;
         } binaryOp;
-
-        struct {
-            char *functionName;
-            struct ExprNode **arguments;
-            int argumentCount;
-            TypeKind returnType;
-        } functionCall;
     } data;
 } ExprNode;
-
-
-typedef struct variableDecNode{
-    char *varName;      // Name of the variable
-    TypeNode *type;     // Pointer to the type node (declared type)
-    struct ExprNode *initialValue; // Expression node for the initial value (can be NULL)
-} VariableDecNode;
 
 #endif
--- muc.c
+++ muc.c
@@ -6,7 +6,6 @@
 #include "linked_list.h"
 #include "tokenizer.h"
 
-//TODO FIx linked_list so it handles void *
 //TODO split typeNode into smaller type nodes(Literals, functions etc.)
 //TODO swap current setup for is_keyword(DONE) and is_operator.
 //TODO add types for nodes in AST
